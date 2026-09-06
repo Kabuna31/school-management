fa2d3a40558843a688da9eac1e887035c2cb40b9
Kabuna Ivan <kabunaivan@gmail.com>
2026-09-06 23:43:44 +0300

    Fix report card template URL namespaces and add delete functionality

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,24 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Import report card views
+from core.views import (
+    ReportCardListView,
+    ReportCardDetailView,
+    ReportCardPrintView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # This adds login/logout URLs
+    
+    # Report Card URLs
+    path('report-cards/', ReportCardListView.as_view(), name='report_card_list'),
+    path('report-cards/<int:student_id>/', ReportCardDetailView.as_view(), name='report_card_detail'),
+    path('report-cards/<int:student_id>/print/', ReportCardPrintView.as_view(), name='report_card_print'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
